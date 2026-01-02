1dc3ea553fd386257b3252305a97199407cd392a
Rajvardhan Singh Rathore <rajvardhansinghrathore1990@gmail.com>
2026-01-02 14:04:10 +0530

    merged new branch origin/features

---
+++ backend/wardsMap.js
@@ -0,0 +1,9 @@
+
+console.log(">>> RUNNING THIS SERVER FILE <<<");
+
+export default [
+  { wardNo: 1, name: "CIVIL LINES" },
+  { wardNo: 2, name: "R K PURAM" },
+  { wardNo: 3, name: "IGI AIRPORT" },
+  { wardNo: 4, name: "DELHI TECHNOLOGICAL UNIVERSITY" }
+];
